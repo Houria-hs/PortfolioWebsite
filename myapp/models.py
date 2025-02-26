e2e1631545789916b7d205e891102a5b69ee0111
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-02-26 22:42:00 +0100

    added DB model for uploading video urls in portfolio section

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video_url = models.URLField(blank=True, null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render , redirect
-
+from django.http import HttpResponse
+from .models import project
 # Create your views here.
 
 def home (request):
-    return render(request , 'index.html' , {})
+    projects = project.objects.all()
+    return render(request, 'home.html' , {'projects': projects})
