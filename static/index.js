9eb95614dc744f648e5a4f54515fbfa66b8b318b
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-03-06 13:40:39 +0100

    Improved style + added some animations

--- static/index.js
+++ static/index.js
@@ -1,6 +1,15 @@
 
-// let hireBtn = document.getElementById('whatsappBtn')
 
-// hireBtn.addEventListener('click', ()=> {
-//     window.open('https://wa.me/2130790492317', '_blank');
-// } )
+const Observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElimnets = document.querySelectorAll('.hidden');
+hiddenElimnets.forEach((el) => Observer.observe(el));
